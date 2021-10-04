b639ca233b94507cf39b5127db0848866b10b0ad
AlexaPim <alexssandraprado@hotmail.com>
2021-10-04 09:45:47 -0300

    CRUD da tabela

---
+++ backend/src/main/java/com/generation/carol/repositories/TemaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.carol.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.carol.entities.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+	public List<Tema> findAllByNomeContainingIgnoreCase(String nome);	
+}
