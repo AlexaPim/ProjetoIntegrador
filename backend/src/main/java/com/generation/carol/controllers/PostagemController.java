b287b5d8f32be04ca19a9fd85416ae4c8631ad79
dbonach <deivitlopes@gmail.com>
2021-10-13 09:28:20 -0300

    Remove @Valid annotation, change put method return
    Also rename repository variable name

--- backend/src/main/java/com/generation/carol/controllers/PostagemController.java
+++ backend/src/main/java/com/generation/carol/controllers/PostagemController.java
@@ -2,8 +2,6 @@ package com.generation.carol.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.carol.entities.Postagem;
-import com.generation.carol.entities.Tema;
 import com.generation.carol.repositories.PostagemRepository;
 
 @RestController
@@ -27,41 +24,36 @@ import com.generation.carol.repositories.PostagemRepository;
 public class PostagemController {
 
 	@Autowired
-	private PostagemRepository repository;
+	private PostagemRepository postagemRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll() {
-		return ResponseEntity.ok(repository.findAll());
+		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getById(@PathVariable long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		return postagemRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
-		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 
 	@PostMapping
-	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
 	}
 
 	@PutMapping
 	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
-		return ResponseEntity.ok(repository.save(postagem));
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
 	}
-	/*
-	 * @PutMapping public ResponseEntity<Postagem> putPostagem (@RequestBody
-	 * Postagem postagem){ return
-	 * ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
-	 */
 
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
-		repository.deleteById(id);
+		postagemRepository.deleteById(id);
 	}
 
 }
--- backend/src/main/java/com/generation/carol/controllers/TemaController.java
+++ backend/src/main/java/com/generation/carol/controllers/TemaController.java
@@ -2,8 +2,6 @@ package com.generation.carol.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,37 +24,37 @@ import com.generation.carol.repositories.TemaRepository;
 public class TemaController {
 
 	@Autowired
-	private TemaRepository repository;
+	private TemaRepository temaRepository;
 
 	@GetMapping
 	public ResponseEntity<List<Tema>> GetAll() {
-		return ResponseEntity.ok(repository.findAll());
+		return ResponseEntity.ok(temaRepository.findAll());
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Tema> getById(@PathVariable long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+		return temaRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/nome/{nome}")
 
 	public ResponseEntity<List<Tema>> getByName(@PathVariable String nome) {
-		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+		return ResponseEntity.ok(temaRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
 
 	@PostMapping
-	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	public ResponseEntity<Tema> post(@RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
 	}
 
 	@PutMapping
 	public ResponseEntity<Tema> put(@RequestBody Tema tema) {
-		return ResponseEntity.ok(repository.save(tema));
+		return ResponseEntity.status(HttpStatus.OK).body(temaRepository.save(tema));
 	}
 
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
-		repository.deleteById(id);
+		temaRepository.deleteById(id);
 	}
 
 }
